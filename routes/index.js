b6a707b488c3084d8c5141a2e0fe3b7accd2cf23
Jason Chiu <jforeza@gmail.com>
2020-03-05 09:47:54 -0800

    Add all placeholder pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Signup' });
+});
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET Dashboard page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
+});
+
 module.exports = router;
