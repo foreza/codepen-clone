90963fcf650eb51fa8bfd391f55887f5478e6bd5
Jason Chiu <jforeza@gmail.com>
2020-03-14 22:09:03 -0700

    Support saving state of pen during resizing, refactor project
    Moved the monaco editor js into pen js
    clean up / add comments

--- public/javascripts/pen.js
+++ public/javascripts/pen.js
@@ -1,28 +1,138 @@
-var leftPane;
-var centerPane;
-var rightPane;
+var leftPane;                                   // Reference to left IDE container
+var centerPane;                                 // Reference to center IDE container
+var rightPane;                                  // Reference to right IDE container
+var bottomPane;                                 // Reference to bottom iframe render container
 
-var leftAnchor;
-var rightAnchor;
+var leftAnchor;                                 // Reference to anchor between left / center IDE container
+var rightAnchor;                                // Reference to anchor between right / center IDE container
+var bottomAnchor;                               // Reference to anchor between IDE group and bottomPane container
 
-var resizeLeft = false;
-var resizeRight = false;
+var resizeLeft = false;                         // Track whether currently resizing with leftAnchor
+var resizeRight = false;                        // Track whether currently resizing with rightAnchor
+var resizeBottom = false;                       // Track whether currently resizing with bottomAnchor
 
-var bottomPane;
-var bottomAnchor;
-var resizeBottom = false;
+var tWidth;                                     // Global store temporary width for left/right anchor operations
 
-var tWidth;
+var leftEditor;                                 // Reference to left monaco editor
+var centerEditor;                               // Reference to center monaco editor
+var rightEditor;                                // Reference to right monaco editor
 
-var tHeight;
+var leftEditorContent;                          // Content for left monaco editor
+var centerEditorContent;                        // Content for center monaco editor
+var rightEditorContent;                         // Content for right monaco editor
 
 
-function refreshEditorElements(){
+// Monaco Editor config
+require.config({ paths: { 'vs': '../javascripts/monaco-editor/min/vs' } });
+
+
+
+function formatDocument(html, css, javascript) {
+    return `<html><head><style>${css} 
+        </style><body>${html} 
+            <script>${javascript}<\/script></body></html>`;
+}
+
+
+function refreshContent() {
+    renderInIframe(formatDocument(leftEditor.getValue(), centerEditor.getValue(), rightEditor.getValue()));
+}
+
+
+function renderInIframe(content) {
+    let iFrame = document.getElementById('results-window');
+    iFrame = iFrame.contentWindow || iFrame.contentDocument.document || iFrame.contentDocument;
+    iFrame.document.open();
+    iFrame.document.write(content);
+    iFrame.document.close();
+}
+
+
+function monaco_configure() {
+    require(['vs/editor/editor.main'], () => {
+        monaco.editor.setTheme('vs-dark');
+    });
+}
+
+// TODO: Allow for an singular function to handle any rendering
+function monaco_loadEditorContent(editorId, content, language) {
+    require(['vs/editor/editor.main'], () => {
+
+        return monaco.editor.create(document.getElementById(editorId), {
+            value: content,
+            language: language
+        });
+
+    });
+}
+
+// TODO: Temporarily set these ravlues
+function monaco_loadContentFromRemote() {
+    
+    leftEditorContent = [
+        '<html><h1>peace in my bodyyy</html>'
+    ].join('\n');
+
+    centerEditorContent = [
+        'body {',
+        '\t margin-left: 0;',
+        '}'
+    ].join('\n');
+
+    rightEditorContent = [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}'
+    ].join('\n');
+}
+
+
+
+function monaco_refreshContent() {
+    require(['vs/editor/editor.main'], function () {
+
+        leftEditor = monaco.editor.create(document.getElementById('editor-1'), {
+            value: leftEditorContent,
+            language: 'html'
+        });
+
+        centerEditor = monaco.editor.create(document.getElementById('editor-2'), {
+            value: centerEditorContent,
+            language: 'css'
+        });
+
+        rightEditor = monaco.editor.create(document.getElementById('editor-3'), {
+            value: rightEditorContent,
+            language: 'javascript'
+        });
+
+
+        leftEditor.onDidChangeModelContent(e => {
+            leftEditorContent = leftEditor.getValue();
+            refreshContent();
+        })
+
+        centerEditor.onDidChangeModelContent(e => {
+            centerEditorContent = centerEditor.getValue();
+            refreshContent();
+        })
+
+        rightEditor.onDidChangeModelContent(e => {
+            rightEditorContent = rightEditor.getValue();
+            refreshContent();
+        })
+
+    });
+}
+
+
+// Called by any resize operation to clear DOM and refresh the editor
+function updateForResize() {
 
     leftPane.empty();
     rightPane.empty();
     centerPane.empty();
-    monaco_refreshWindow();
+    monaco_refreshContent();
 
 }
 
@@ -36,78 +146,74 @@ $(function () {
     centerPane = $("#editor-2");
     rightPane = $("#editor-3");
 
-    editorPane = $("#editor-group");    
+    editorPane = $("#editor-group");
     bottomPane = $("#result-group");
-
 
     bottomPane.height(window.innerHeight - editorPane.height());
 
     bottomAnchor.mousedown(() => {
-        console.log("left pressed down");
         resizeLeft = resizeRight = false;
         resizeBottom = true;
     });
 
     leftAnchor.mousedown(() => {
-        console.log("left pressed down");
+        resizeRight = resizeBottom = false;
         resizeLeft = true;
-        resizeRight = false;
-        resizeBottom = false;
         tWidth = leftPane.width() + centerPane.width();
     });
 
     rightAnchor.mousedown(() => {
-        console.log("right pressed down");
         resizeRight = true;
-        resizeLeft = false;
-        resizeBottom = false;
+        resizeLeft = resizeBottom = false;
         tWidth = centerPane.width() + rightPane.width();
     });
 
     $(document).mouseup(() => {
-        console.log("release");
         resizeLeft = resizeRight = resizeBottom = false;
     })
 
+
     $(document).on('mousemove', (e) => {
 
-        if (resizeLeft) {
+        if (resizeLeft || resizeRight || resizeBottom) {
 
-            var val = (e.pageX / window.innerWidth) * 100;
-            leftPane.width(`${val}%`);
+            if (resizeLeft) {
+                const val = (e.pageX / window.innerWidth) * 100;
+                leftPane.width(`${val}%`);
+                const val2 = ((tWidth / window.innerWidth) * 100) - val;
+                centerPane.width(`${val2}%`);
+            }
 
-            var val2 = ((tWidth / window.innerWidth) * 100) - val;
-            centerPane.width(`${val2}%`);
+            else if (resizeRight) {
+                const val = ((e.pageX - leftPane.width()) / window.innerWidth) * 100;
+                centerPane.width(`${val}%`);
+                const val2 = ((tWidth / window.innerWidth) * 100) - val;
+                rightPane.width(`${val2}%`);
+            }
 
-            refreshEditorElements();
-        }
+            else if (resizeBottom) {
+                const val = window.innerHeight - e.pageY;
+                bottomPane.height(`${val}px`);
+                editorPane.height(`${e.pageY}px`);
+            }
 
-        if (resizeRight) {
-
-            var val = ((e.pageX - leftPane.width()) / window.innerWidth) * 100;
-            centerPane.width(`${val}%`);
-
-            var val2 = ((tWidth / window.innerWidth) * 100) - val;
-            rightPane.width(`${val2}%`);
-
-            refreshEditorElements();
-        }
-
-        if (resizeBottom) {
-
-            var val = window.innerHeight - e.pageY;
-            bottomPane.height(`${val}px`);
-            editorPane.height(`${e.pageY}px`);
-
-            refreshEditorElements();
+            updateForResize();
 
         }
+
+
+
 
 
 
 
     });
 
+
+
+    monaco_configure();
+    monaco_loadContentFromRemote();
+    monaco_refreshContent();
 
 });
 
