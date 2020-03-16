3306549155f0e0a8691dcc0a30d3da701460b16b
Thi Nguyen <thiman@gmail.com>
2020-03-15 22:07:21 -0700

    sequelize stuff

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", async (req, res) => {
+    const insertQuery = `INSERT INTO "Users"
+                        ("Username", "Email", "Password", "CreatedDate")
+                        VALUES
+                        (:username, :email, :password, :date);`;
+
+    try {
+        const newUserResult = await db.sequelize
+            .query(
+                insertQuery,
+                {
+                    replacements: {
+                        ...req.body,
+                        date: new Date,
+                    },
+                    type: db.sequelize.QueryTypes.INSERT
+                }
+            );
+        res.status(201).json(newUserResult);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
 module.exports = router;
