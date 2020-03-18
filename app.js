891b2c4a24aa1fdf3a6e8904cbd8767be6e0989a
Jason Chiu <jforeza@gmail.com>
2020-03-17 22:59:23 -0700

    Add missing dependency, support db connection

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const db = require('./models/index')
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// postgres connect
+db.sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch (error => {
+  console.error('Unable to connect to the database:', error);
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
