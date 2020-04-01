06754e44d44d09e5f5cc2064ba96df9c16bf95de
Jason Chiu <jforeza@gmail.com>
2020-03-31 23:38:53 -0700

    Modify pen utils to fit new table scheme

--- dbUtils/penUtils.js
+++ dbUtils/penUtils.js
@@ -1,14 +1,36 @@
 var db = require('../models/index')
 const util = {};
 
+/* 
+    CREATE PEN: 
+    Pens belong to a User, and are associated based off of userId.
+
+    Required Params:
+    * userId (fkey)      
+    * penName
+    * hashId
+
+    Optional Params can be null (indicating empty)
+    * numFavorites
+    * numComments
+    * numViews
+*/
+
+// INSERT INTO "Pens" ( 
+//     "userId", "penName", "hashId", "numFavorites",  "numComments", "numViews") 
+// VALUES (1, 'basic bob', 'A9A9A9A9', 40,  20, 999)
+// RETURNING *;
+
+
+// TODO: Use a transaction to ensure that Pen Creation is coupled with Pen Fragment creation
 
 const addNewPenQuery = `INSERT INTO "Pens" ( 
-    "userId", "penName", "cssContent", "jsContent",  "htmlContent") 
-VALUES (:userId, :penName, :cssContent, :jsContent,  :htmlContent)
+    "userId", "penName", "hashId", "numFavorites",  "numComments", "numViews") 
+VALUES (:userId, :penName, :hashId, :numFavorites,  :numComments, :numViews)
 RETURNING *;`
 util.addNewPen = async (pen) => {
     const ret = await db.sequelize.query(addNewPenQuery, {
-        replacements: { ...pen},
+        replacements: { ...pen },
         type: db.sequelize.QueryTypes.INSERT
     });
 
@@ -16,12 +38,33 @@ util.addNewPen = async (pen) => {
 }
 
 
-const updatePenContentByPenIDQuery = (update) => { return `UPDATE "Pens" 
-SET "penName"='${update.penName}', "cssContent"='${update.cssContent}', "jsContent"='${update.jsContent}', "htmlContent"='${update.htmlContent}'
+
+/* 
+    UPDATE PEN: 
+    We can update the name of the pen, as well as any of the optional params.
+
+    Required Params:
+    * penId (primary key)      
+    * penName
+
+    Optional Params can be null (indicating empty)
+    * numFavorites
+    * numComments
+    * numViews
+*/
+
+// UPDATE "Pens" 
+// SET "penName"='a whole new world', "numFavorites"=50, "numComments"=10, "numViews"=568
+// WHERE ("penId"=4)
+// RETURNING *;
+
+const updatePenInfoByPenIDQuery = (update) => {
+    return `UPDATE "Pens" 
+SET "penName"='${update.penName}', "numFavorites"='${update.cssContent}', "numComments"='${update.jsContent}', "numViews"='${update.htmlContent}'
 WHERE ("penId"=${update.penId})
 RETURNING *;` };
-util.updatePenContentByPenID = async (pen) => {
-    const ret = await db.sequelize.query(updatePenContentByPenIDQuery(pen), {
+util.updatePenContentByPenID = async (update) => {
+    const ret = await db.sequelize.query(updatePenInfoByPenIDQuery(update), {
         type: db.sequelize.QueryTypes.UPDATE
     });
 
@@ -29,18 +72,37 @@ util.updatePenContentByPenID = async (pen) => {
 }
 
 
+/* 
+    GET PEN: 
+    We can retrieve a pen given the id
+
+    Required Params:
+    * penId (primary key)      
+*/
+
+// SELECT * FROM "Pens" WHERE ("penId"=4);
+
 const getPenByPenIDQuery = (id) => { return `SELECT * FROM "Pens" WHERE ("penId"=${id});` };
 util.getPenByPenID = (id) => db.sequelize.query(getPenByPenIDQuery(id), {
     type: db.sequelize.QueryTypes.SELECT,
 });
 
 
+/* 
+    GET PENS associated with User : 
+    We can retrieve a set of User Pens associated with a userId
+
+    Required Params:
+    * userId (fkey)   
+    * count (# of pens to return)   
+*/
+
+// SELECT * FROM "Pens" WHERE ("userId"=1) LIMIT 10;
+
 const getXPensByUserIDQuery = (userId, count) => { return `SELECT * FROM "Pens" WHERE ("userId"=${userId}) LIMIT ${count};` };
 util.getPenByUserID = (userId, count) => db.sequelize.query(getXPensByUserIDQuery(userId, count), {
     type: db.sequelize.QueryTypes.SELECT
 });
-
-
 
 
 module.exports = util;
