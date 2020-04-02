10251898e37c6d88f4129e369370b9c029022d6b
Jason Chiu <jforeza@gmail.com>
2020-04-02 16:51:18 -0700

    Hook in dashboard to show num fav/view/comment

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -31,19 +31,19 @@ function renderPensForUser(pens) {
 
     for (var i = 0; i < pens.length; ++i){
         console.log("creating pen: ", pens[i].penName);
-        $("#pen-card-view").append(generatePenCardDom(pens[i].penName, pens[i].userId, pens[i].penId));
+        $("#pen-card-view").append(generatePenCardDom(pens[i]));
     }
 
 }
 
 
 
-function generatePenCardDom(title, userId, cardId){
+function generatePenCardDom(pen){
 
     return `<div class="col s12 m6 l4">
                 <div class="card">
                     <div class="card-image">
-                        <a class="pen-title-link" href="/${userId}/pen/${cardId}">
+                        <a class="pen-title-link" href="/${pen.hashId}/pen/${pen.penId}">
                             <div class="placeholder">
                             </div>
                         </a>   
@@ -51,17 +51,17 @@ function generatePenCardDom(title, userId, cardId){
                 </div>
                 <div class="card-content">
                     <div class="row card-info">
-                    <a class="pen-title-link" href="/${userId}/pen/${cardId}">
-                        <span>${title}</span>
+                    <a class="pen-title-link" href="/${pen.hashId}/pen/${pen.penId}">
+                        <span>${pen.penName}</span>
                     </a>
                     <span class="card-options">
                         <i class="material-icons">more_horiz</i>
                     </span>
                     </div>
                     <div class="row card-action">
-                        <button><i class="material-icons">favorite_border</i><span>4</span></button>
-                        <button><i class="material-icons">comment</i><span>3</span></button>
-                        <button><i class="material-icons">visibility</i><span>2</span></button>
+                        <button><i class="material-icons">favorite_border</i><span>${pen.numFavorites}</span></button>
+                        <button><i class="material-icons">comment</i><span>${pen.numComments}</span></button>
+                        <button><i class="material-icons">visibility</i><span>${pen.numViews}</span></button>
                     </div>
                 </div>
             </div>`
