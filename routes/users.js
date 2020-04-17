340dc69181ab2a2eb30f6a997de6a7be08979cf3
Jason Chiu <jforeza@gmail.com>
2020-04-16 23:14:38 -0700

    Cleanup users route

--- routes/users.js
+++ routes/users.js
@@ -5,60 +5,67 @@ var collections = require('../middleware/collections')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
 /* GET user by ID */
-router.get('/:id', async function (req, res, next) {
-
-  console.log("Lookup by this id: ", req.params.id);
-  const user = await userUtil.getUserById(req.params.id);
-
-  if (!user) {
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await userUtil.getUserById(req.params.id);
+    if (!user) {
+      throw Error("User object empty");
+    } else {
+      res.json(user)
+    }
+  } catch (err) {
+    console.error(`Get User failed: ${err}`)
     res.sendStatus(404);
-  } else {
-    res.json(user)
   }
-
 });
 
 /* POST new user */ 
-router.post('/', [collections.validateUserCreation], async function (req, res, next) {
+router.post('/', [collections.validateUserCreation], (req, res, next) => {
   const user = req.body;
   bcrypt.hash(user.password, saltRounds, async (err, hash) => {
     user.password = hash;
-    const newUser = await userUtil.addUser(user);
-    console.log(newUser[0][0]);
-    res.status(201).json(newUser[0][0]);
+    try {
+      const newUser = await userUtil.addUser(user);
+      if (!newUser) {
+        throw Error("New user object empty")
+      } else {
+        res.status(201).json(newUser[0][0]);
+      }
+    } catch (err) {
+      console.error(`POST User failed: ${err}`)
+      res.sendStatus(400);
+    }
+
   });
 });
 
-router.post('/login', [collections.validateUserLogin], async function (req, res, next) {
-
-  const fetchedUser = await userUtil.checkValidUser(req.body);
-
-  if (fetchedUser.length === 0) {
-    res.sendStatus(401);
-  } else {
-    const result = await bcrypt.compare(req.body.password, fetchedUser[0].password);
-    if (result) {
-      req.session.user = fetchedUser[0];
-      res.sendStatus(200);
+/* LOGIN user */ 
+router.post('/login', [collections.validateUserLogin], async (req, res, next) => {
+  try {
+    const fetchedUser = await userUtil.checkValidUser(req.body);
+    if (fetchedUser.length === 0) {
+      throw Error("Fetched User is invalid")
     } else {
-      res.sendStatus(401);
+      const result = await bcrypt.compare(req.body.password, fetchedUser[0].password);
+      if (result) {
+        req.session.user = fetchedUser[0];
+        res.sendStatus(200);
+      } else {
+        throw Error("Invalid credentials")
+      }
     }
-
+  } catch (err) {
+    console.error(`LOGIN User failed: ${err}`)
+    res.sendStatus(401);
   }
+ 
 
 });
 
-
-router.get('/', function (req, res, next) {
+/* Default */ 
+router.get('/', (req, res, next) => {
   res.sendStatus(200);
 });
-
-
-
-
-
-
 
 module.exports = router;
