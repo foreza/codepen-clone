255c52a096148b4b52b3d60ac7efd61db007e8f2
Jason Chiu <jforeza@gmail.com>
2020-04-17 16:56:31 -0700

    Cleanup/refactor userUtils to use replacement

--- dbUtils/userUtils.js
+++ dbUtils/userUtils.js
@@ -1,7 +1,12 @@
 var db = require('../models/index')
 const util = {};
 
-const addUserQuery = `INSERT INTO "Users" ( "fullName", "username", "email", "password", "createdAt") VALUES (:fullName, :username, :email, :password, :createdAt)RETURNING *;`
+const addUserQuery = 
+`INSERT INTO "Users" 
+("fullName", "username", "email", "password", "createdAt") 
+VALUES (:fullName, :username, :email, :password, :createdAt)
+RETURNING *;`
+
 util.addUser = async (user) => {
     const ret = await db.sequelize.query(addUserQuery, {
         replacements: { ...user, createdAt: new Date() },
@@ -11,33 +16,23 @@ util.addUser = async (user) => {
     return ret;
 }
 
-const checkValidUserQuery = `SELECT * FROM "Users" 
-                        WHERE (
-                            email=:usernameOrEmail OR 
-                            username=:usernameOrEmail
-                            ) 
-                        LIMIT 1;`
+const checkValidUserQuery = 
+`SELECT * FROM "Users" WHERE 
+(email=:usernameOrEmail OR username=:usernameOrEmail) 
+LIMIT 1;`
+
 util.checkValidUser = (user) => db.sequelize.query(checkValidUserQuery, {
     replacements: { ...user },
     type: db.sequelize.QueryTypes.SELECT
 });
 
+const getUserByIdQuery = 
+`SELECT * FROM "Users" WHERE 
+(id=:id);`;
 
-const getUserByIdQuery = (id) => { return `SELECT * FROM "Users" WHERE (id=${id});` };
-util.getUserById = (id) => db.sequelize.query(getUserByIdQuery(id), {
+util.getUserById = (id) => db.sequelize.query(getUserByIdQuery, {
+    replacements: { id: id },
     type: db.sequelize.QueryTypes.SELECT
 });
-
-// TODO: flesh this out and plug in
-
-util.getUserByUserName = (name) => db.sequelize.query(`SELECT * FROM "Users" WHERE (username=${name});`, {
-    type: db.sequelize.QueryTypes.SELECT
-});
-
-util.getUserByEmail = (name) => db.sequelize.query(`SELECT * FROM "Users" WHERE (email=${email});`, {
-    type: db.sequelize.QueryTypes.SELECT
-});
-
-
 
 module.exports = util;
